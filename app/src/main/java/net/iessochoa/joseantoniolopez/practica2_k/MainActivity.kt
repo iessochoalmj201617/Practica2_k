bd18248f30d92c35276f2c2f1b64052df22b489a
JoseA <iessochoalmj201617@gmail.com>
2022-04-01 11:48:36 +0200

    con agenda

--- app/src/main/java/net/iessochoa/joseantoniolopez/practica2_k/MainActivity.kt
+++ app/src/main/java/net/iessochoa/joseantoniolopez/practica2_k/MainActivity.kt
@@ -1,11 +1,17 @@
 package net.iessochoa.joseantoniolopez.practica2_k
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btAgenda:Button=findViewById(R.id.btnEjercicio1)
+        btAgenda.setOnClickListener(){
+            startActivity(Intent(this@MainActivity,AgendaActivity::class.java))
+        }
     }
 }
