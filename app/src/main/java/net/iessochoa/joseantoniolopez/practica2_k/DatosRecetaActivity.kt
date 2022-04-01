1d7d30b91fed252c4e0bc2e9bab49829e0cbf9c2
JoseA <iessochoalmj201617@gmail.com>
2022-04-01 17:55:47 +0200

    temp

---
+++ app/src/main/java/net/iessochoa/joseantoniolopez/practica2_k/DatosRecetaActivity.kt
@@ -0,0 +1,11 @@
+package net.iessochoa.joseantoniolopez.practica2_k
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatosRecetaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos_receta)
+    }
+}
