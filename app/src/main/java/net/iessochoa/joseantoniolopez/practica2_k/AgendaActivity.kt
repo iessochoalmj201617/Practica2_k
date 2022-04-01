4a239d32bcca2270912328742b6b9eabd42f1139
JoseA <iessochoalmj201617@gmail.com>
2022-04-01 13:30:12 +0200

    fin agenda

--- app/src/main/java/net/iessochoa/joseantoniolopez/practica2_k/AgendaActivity.kt
+++ app/src/main/java/net/iessochoa/joseantoniolopez/practica2_k/AgendaActivity.kt
@@ -2,10 +2,36 @@ package net.iessochoa.joseantoniolopez.practica2_k
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class AgendaActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agenda)
+
+        val btAceptar: Button = findViewById(R.id.btAceptar)
+        btAceptar.setOnClickListener() { accionAceptar() }
+
+        val btCancelar: Button = findViewById(R.id.btCancelar)
+        btCancelar.setOnClickListener() { finish() }
+
     }
+
+
+    private fun accionAceptar() {
+        val etNombre: EditText = findViewById(R.id.etNombre)
+        val etApellido: EditText = findViewById(R.id.etApellido)
+        val tvTitulo:TextView=findViewById(R.id.tvTitulo)
+        if(etNombre.text.toString().isEmpty() || etApellido.text.toString().isEmpty())
+            Toast.makeText(this@AgendaActivity,"Es necesario introducir el Nombre y el Apellido",Toast.LENGTH_LONG).show()
+        else
+            tvTitulo.text=etNombre.text.toString()+ " "+etApellido.text.toString()
+
+
+    }
+
 }
